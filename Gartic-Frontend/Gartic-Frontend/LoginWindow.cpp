e7ef0a1e4e25079e767960363fd0ad3d33cd40a6
IuliaTipeiu <ana.tipeiu@student.unitbv.ro>
2023-10-28 21:58:13 +0300

    Validate login input

--- Gartic-Frontend/Gartic-Frontend/LoginWindow.cpp
+++ Gartic-Frontend/Gartic-Frontend/LoginWindow.cpp
@@ -1,10 +1,59 @@
 #include "LoginWindow.h"
+#include <QRegularExpression> 
+#include <QMessageBox> 
 
 LoginWindow::LoginWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+    connect(ui.LoginButton, &QPushButton::clicked, this, &LoginWindow::OnLoginButtonClicked);
 }
 
 LoginWindow::~LoginWindow()
 {}
+
+void LoginWindow::OnLoginButtonClicked() 
+{
+    QString email = ui.EmailInput->text();
+    QString password = ui.PasswordInput->text();
+
+    if (ValidateCredentials()) 
+    {
+        // Attempt to login or any other logic
+    }
+    else 
+    {
+        // Show error or validation message
+    }
+}
+
+bool LoginWindow::IsValidEmail(const QString& email) 
+{
+    
+    QRegularExpression regex("^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4})*$");
+
+   
+    return regex.match(email).hasMatch();
+}
+
+
+bool LoginWindow::ValidateCredentials() 
+{
+    QString email = ui.EmailInput->text();
+    QString password = ui.PasswordInput->text();
+    
+    if (IsValidEmail(email) == false) 
+    {
+        QMessageBox::warning(this, "Invalid", "Email is not valid!");
+        return false;
+    }
+
+    if (password.isEmpty() == true)
+    {
+        QMessageBox::warning(this, "Invalid", "Password is not valid!");
+        return false;
+    }
+
+    return true;
+}
--- Gartic-Frontend/Gartic-Frontend/LoginWindow.h
+++ Gartic-Frontend/Gartic-Frontend/LoginWindow.h
@@ -3,6 +3,7 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_LoginWindow.h"
 
+
 class LoginWindow : public QMainWindow
 {
 	Q_OBJECT
@@ -11,6 +12,13 @@ public:
 	LoginWindow(QWidget *parent = nullptr);
 	~LoginWindow();
 
+	bool IsValidEmail(const QString& email); 
+
 private:
 	Ui::LoginWindowClass ui;
+	bool ValidateCredentials();
+
+private slots:
+	void OnLoginButtonClicked();
+
 };
