a2d5646d8194bc9cdb601a3bde276eff078562a6
rusuuuu <claudiurusu_2003@yahoo.com>
2023-11-26 03:01:15 +0200

    Partially designed ProfileUserWindow

--- Gartic-Frontend/Gartic-Frontend/ProfileUserWindow.cpp
+++ Gartic-Frontend/Gartic-Frontend/ProfileUserWindow.cpp
@@ -9,15 +9,18 @@ ProfileUserWindow::ProfileUserWindow(QWidget *parent)
 	connect(ui.BackToMainMenuFromProfileUser, &QPushButton::clicked, this, &ProfileUserWindow::OnBackToMainMenuClicked);
     connect(ui.UploadButton, &QPushButton::clicked, this, &ProfileUserWindow::UploadAvatar);
 
-    ui.ProfileNameLabel->setText(ProfileName);
-    ui.ProfileNameLabel->setText(TotalGames);
-    ui.ProfileNameLabel->setText(GamesWon);
-    ui.ProfileNameLabel->setText(GamesLost);
-    ui.ProfileNameLabel->setText(TotalPoints);
+    ui.ProfileName->setText(ProfileName);
+    ui.TotalGames->setText(TotalGames);
+    ui.GamesWon->setText(GamesWon);
+    ui.GamesLost->setText(GamesLost);
+    ui.TotalPoints->setText(TotalPoints);
+
+    SetDefaultAvatar();
 }
 
 ProfileUserWindow::~ProfileUserWindow()
-{}
+{
+}
 
 void ProfileUserWindow::OnBackToMainMenuClicked()
 {
@@ -30,7 +33,20 @@ void ProfileUserWindow::UploadAvatar()
 
     if (!imagePath.isEmpty()) {
         QPixmap avatar(imagePath);
-        ui.ProfilePictureLabel->setPixmap(avatar);
-        ui.ProfilePictureLabel->setScaledContents(true);
+        ui.ProfilePicture->setPixmap(avatar);
+        ui.ProfilePicture->setScaledContents(true);
     }
+}
+
+void ProfileUserWindow::SetDefaultAvatar()
+{
+    // Set the path to your default avatar image
+    QString defaultImagePath = ":/GarticFrontend/Images/profile_icon.png";
+
+    QPixmap defaultAvatar(defaultImagePath);
+    ui.ProfilePicture->setPixmap(defaultAvatar);
+    ui.ProfilePicture->setScaledContents(true);
+
+    // Set the QLabel as a circle
+    ui.ProfilePicture->setStyleSheet("QLabel { border-radius: 50%; background-color: transparent; }");
 }
--- Gartic-Frontend/Gartic-Frontend/ProfileUserWindow.h
+++ Gartic-Frontend/Gartic-Frontend/ProfileUserWindow.h
@@ -5,6 +5,7 @@
 
 #include <QWidget>
 #include <QLabel>
+#include <QPainter>
 #include <QPushButton>
 #include <QFileDialog>
 #include <QVBoxLayout>
@@ -19,6 +20,7 @@ public:
 	~ProfileUserWindow();
 
 	void OnBackToMainMenuClicked();
+	void SetDefaultAvatar();
 
 signals:
 	void ShowMainMenuWindow();
