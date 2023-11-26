006008d97085b24b21c216b45ac87efbdca09de7
rusuuuu <claudiurusu_2003@yahoo.com>
2023-11-26 04:17:34 +0200

    Createed one main window and loaded the other windows inside the main one

--- Gartic-Frontend/Gartic-Frontend/GarticFrontend.h
+++ Gartic-Frontend/Gartic-Frontend/GarticFrontend.h
@@ -3,6 +3,14 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_GarticFrontend.h"
 
+#include <QPropertyAnimation>
+
+#include "LoginWindow.h"
+#include "RegisterWindow.h"
+#include "MainMenuWindow.h"
+#include "HostRoom.h"
+#include "ProfileUserWindow.h"
+
 class GarticFrontend : public QMainWindow
 {
     Q_OBJECT
