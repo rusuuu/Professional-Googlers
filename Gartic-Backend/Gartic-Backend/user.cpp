9e863bde67517e77fb966ad2b46ac343755e2286
IuliaTipeiu <ana.tipeiu@student.unitbv.ro>
2023-12-06 20:01:42 +0200

    Updated user module and implemented necessary methods

--- Gartic-Backend/Gartic-Backend/user.cpp
+++ Gartic-Backend/Gartic-Backend/user.cpp
@@ -1,14 +1,26 @@
 #include "user.cppm"
 
-namespace gartic
+#include <utility>
+
+namespace gartic 
 {
+    // Constructors
     User::User() : m_role(Role::NoRole) {}
 
-    User::User(const std::string& userName, Role userRole) : m_name(userName), m_role(userRole) {}
+    User::User(std::string userName, std::string userEmail, std::string userPassword, std::string userPicture, Role userRole)
+        : m_name(std::move(userName)), m_email(std::move(userEmail)), m_password(std::move(userPassword)),
+        m_picture(std::move(userPicture)), m_role(userRole) {}
 
-    User::User(const User& other) = default;
+    // Rule of Five
+    User::User(const User& other)
+        : m_name(other.m_name), m_email(other.m_email), m_password(other.m_password),
+        m_picture(other.m_picture), m_role(other.m_role), m_statistics(other.m_statistics) {}
 
-    User::User(User&& other) noexcept = default;
+    User::User(User&& other) noexcept
+        : User()
+    {
+        swap(*this, other);
+    }
 
     User& User::operator=(User other)
     {
@@ -16,28 +28,57 @@ namespace gartic
         return *this;
     }
 
-    User::~User() = default;
+    User::~User() {}
 
+    // Member functions
     void swap(User& first, User& second) noexcept
     {
         using std::swap;
         swap(first.m_name, second.m_name);
+        swap(first.m_email, second.m_email);
+        swap(first.m_password, second.m_password);
+        swap(first.m_picture, second.m_picture);
         swap(first.m_role, second.m_role);
+        swap(first.m_statistics, second.m_statistics);
     }
 
-    const std::string& User::GetName() const
-    {
-        return m_name;
-    }
-
-    User::Role User::GetRole() const
-    {
-        return m_role;
-    }
-
-    std::ostream& operator<<(std::ostream& os, const User& user)
+    std::ostream& operator<< (std::ostream& os, const User& user)
     {
         os << "User: " << user.m_name << ", Role: " << static_cast<int>(user.m_role);
         return os;
+    }
+
+    const std::string& User::GetName() const 
+    {
+        return m_name; 
+    }
+    const std::string& User::GetEmail() const
+    {
+        return m_email; 
+    }
+    const std::string& User::GetPassword() const 
+    {
+        return m_password; 
+    }
+    const std::string& User::GetPicture() const 
+    {
+        return m_picture; 
+    }
+    User::Role User::GetRole() const 
+    {
+        return m_role; 
+    }
+    User::Statistics User::GetStatistics() const 
+    {
+        return m_statistics; 
+    }
+
+    void User::SetRole(Role newRole)
+    { 
+        m_role = newRole;
+    }
+    void User::UpdateStatistics(const Statistics& stats) 
+    { 
+        m_statistics = stats; 
     }
 }
--- Gartic-Backend/Gartic-Backend/user.cppm
+++ Gartic-Backend/Gartic-Backend/user.cppm
@@ -2,6 +2,7 @@ export module user;
 
 #include <string>
 #include <iostream>
+#include <utility>
 
 export namespace gartic
 {
@@ -15,8 +16,16 @@ export namespace gartic
             NoRole
         };
 
+        struct Statistics
+        {
+            int gamesPlayed = 0;
+            int gamesWon = 0;
+            int correctGuesses = 0;
+        };
+
+        // Constructors
         User();
-        User(const std::string& userName, Role userRole);
+        User(std::string userName, std::string userEmail, std::string userPassword, std::string userPicture, Role userRole = Role::NoRole);
 
         // Rule of Five
         User(const User& other);
@@ -25,16 +34,26 @@ export namespace gartic
 
         ~User();
 
-        // For copy-and-swap idiom:
+        // Member functions
         friend void swap(User& first, User& second) noexcept;
+        friend std::ostream& operator<< (std::ostream& os, const User& user);
 
         const std::string& GetName() const;
+        const std::string& GetEmail() const;
+        const std::string& GetPassword() const; // Consider hashing passwords for security
+        const std::string& GetPicture() const;
         Role GetRole() const;
+        Statistics GetStatistics() const;
 
-        friend std::ostream& operator<< (std::ostream& os, const User& user);
+        void SetRole(Role newRole);
+        void UpdateStatistics(const Statistics& stats);
 
     private:
         std::string m_name;
+        std::string m_email;
+        std::string m_password; // Store hashed password
+        std::string m_picture;
         Role m_role;
+        Statistics m_statistics;
     };
 }
