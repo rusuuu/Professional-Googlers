2caf675420dd66b7b00b1a01be06e4b077c6c10e
alexandrudecean <alexandru.decean@student.unitbv.ro>
2023-12-22 18:14:12 +0200

    host game implementation in progress

---
+++ Server/Server/GameHandler.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "crow.h"
+#include "Database.h"
+#include "User.h"
+//#include "Game.h"
+
+class GameHandler
+{
+public:
+    GameHandler(Database::Storage& db);
+
+    crow::response HostGame(const crow::request& req);
+    crow::response GetGames();
+    crow::response GetGameById(const crow::request& req);
+
+private:
+    Database::Storage& m_db;
+};
+
